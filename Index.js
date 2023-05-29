f85f557b5694d7895198c0498bdf93a6b697ced6
imruben2991@gmail.com <im.ruben2991@gmail.com>
2023-05-29 12:56:28 -0600

    Se agrega el Proyecto

--- Index.js
+++ Index.js
@@ -1,41 +1,69 @@
-   // Función para encriptar el texto
-   function encryptText(text) {
-    text = text.toLowerCase();
+function encryptText() {
+  const inputText = document.getElementById("texto").value;
+  const encryptStatus = document.getElementById("titulo-mensaje");
+  const message = document.getElementById("parrafo");
+  const image = document.getElementById("muñeco");
 
-    text = text.replace(/e/g, "enter");
-    text = text.replace(/i/g, "imes");
-    text = text.replace(/a/g, "ai");
-    text = text.replace(/o/g, "ober");
-    text = text.replace(/u/g, "ufat");
+  if (inputText.length !== 0) {
+      const encryptedText = inputText
+          .replace(/e/gi, "enter")
+          .replace(/i/gi, "imes")
+          .replace(/a/gi, "ai")
+          .replace(/o/gi, "ober")
+          .replace(/u/gi, "ufat");
 
-    return text;
+      document.getElementById("textoEncriptado").value = encryptedText;
+
+      encryptStatus.textContent = "Texto encriptado con éxito";
+      message.textContent = "";
+      image.src = "./img/encriptado.png";
+
+  } else {
+      image.src = "./img/muñeco.png";
+      encryptStatus.textContent = "Ningún mensaje fue encontrado";
+      message.textContent = "Ingresa el texto que deseas encriptar o desencriptar.";
+      alert("Ooops! Debes ingresar un texto.");
   }
+}
 
-  // Función para desencriptar el texto
-  function decryptText(text) {
-    text = text.toLowerCase();
 
-    text = text.replace(/enter/g, "e");
-    text = text.replace(/imes/g, "i");
-    text = text.replace(/ai/g, "a");
-    text = text.replace(/ober/g, "o");
-    text = text.replace(/ufat/g, "u");
+function decryptText() {
+  const inputText = document.getElementById("texto").value;
+  const decryptStatus = document.getElementById("titulo-mensaje");
+  const message = document.getElementById("parrafo");
+  const image = document.getElementById("muñeco");
 
-    return text;
+  if (inputText.length !== 0) {
+      document.getElementById("textoEncriptado").value = inputText
+          .replace(/enter/gi, "e")
+          .replace(/imes/gi, "i")
+          .replace(/ai/gi, "a")
+          .replace(/ober/gi, "o")
+          .replace(/ufat/gi, "u");
+
+      decryptStatus.textContent = "Texto desencriptado con éxito";
+      message.textContent = "";
+      image.src = "./img/desencriptado.png";
+  } else {
+      image.src = "./img/muñeco.png";
+      decryptStatus.textContent = "Ningún mensaje fue encontrado";
+      message.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      alert("Ooops! Debes ingresar un texto.");
   }
+}
 
-  // Función para procesar el texto según la opción seleccionada
-  function processText() {
-    var option = document.querySelector('input[name="option"]:checked').value;
-    var inputText = document.getElementById("input-text").value;
-    var result = document.getElementById("result");
+function copyToClipboard() {
+  const textoEncriptado = document.getElementById("textoEncriptado").value;
 
-    var processedText;
-    if (option === "encrypt") {
-      processedText = encryptText(inputText);
-    } else {
-      processedText = decryptText(inputText);
-    }
-
-    result.textContent = processedText;
+  if (textoEncriptado.length !== 0) {
+      navigator.clipboard.writeText(textoEncriptado)
+          .then(() => {
+              alert("Texto copiado al portapapeles");
+          })
+          .catch((error) => {
+              console.error("Error al copiar al portapapeles:", error);
+          });
+  } else {
+      alert("No hay texto para copiar");
   }
+}
